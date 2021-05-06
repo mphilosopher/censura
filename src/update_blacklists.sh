8618f54d7ba805e3483e07125edabc5b96fb5f09
Mauro Angiolillo <mauro.angiolillo@gmail.com>
2021-05-06 16:48:45 +0200

    Fixed update_admt.sh call

--- src/update_blacklists.sh
+++ src/update_blacklists.sh
@@ -4,7 +4,7 @@
 
 ${ROOT_DIR}/update_cncpo.sh
 ${ROOT_DIR}/update_aams.sh
-${ROOT_DIR}/update_agcom.sh
+${ROOT_DIR}/update_admt.sh
 ${ROOT_DIR}/update_manual.sh
 
 /usr/local/etc/rc.d/unbound reload
