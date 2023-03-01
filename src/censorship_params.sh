32063bd7bf384a34e04d0fad818a6084c6832c5e
Enrico <crs.enrico@gmail.com>
2023-03-01 15:40:21 +0100

    Update censorship_params.sh

--- src/censorship_params.sh
+++ src/censorship_params.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -e
 # Tune the following parameters according to your environment
 
-ROOT_DIR="/root/censorship/src"
+ROOT_DIR="/root/censura/src"
 TMP_DL_DIR="${ROOT_DIR}/tmp"
 PARSER_BIN="${ROOT_DIR}/censor_parser.py"
 WGET_BIN=$(which wget)
@@ -10,7 +10,7 @@ BLACKHOLE="127.0.0.1" #Replace with the chosen IP address
 OUTPUT_FORMAT="unbound"  # Replace to "bind" or to "unbound"
 
 # Unbound params
-UNBOUND_CONF_DIR="/usr/local/etc/unbound/blacklists.d"
+UNBOUND_CONF_DIR="/etc/unbound/unbound.conf.d"
 
 # Bind params
 BIND_CONF_DIR="/etc/bind/zones/blacklist.d"
