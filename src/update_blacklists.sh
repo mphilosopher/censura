a2014a861bbcc1ce56a42bb6fd27f4927dd4ed4f
Mauro Angiolillo <morpheus@mindcreations.it>
2023-04-13 13:16:59 +0200

    FIX - Continue on download errors

--- src/update_blacklists.sh
+++ src/update_blacklists.sh
@@ -2,9 +2,9 @@
 
 . $(dirname "${0}")/censorship_params.sh
 
-${ROOT_DIR}/update_cncpo.sh
-${ROOT_DIR}/update_aams.sh
-${ROOT_DIR}/update_admt.sh
-${ROOT_DIR}/update_manual.sh
+${ROOT_DIR}/update_cncpo.sh || true
+${ROOT_DIR}/update_aams.sh  || true
+${ROOT_DIR}/update_admt.sh  || true
+${ROOT_DIR}/update_manual.sh  || true
 
 /usr/local/etc/rc.d/unbound reload # or `rndc reload` according to your mileage
