0383e44087288a68aa86a4ea48d74bf9102a73d6
Mauro Angiolillo <morpheus@mindcreations.it>
2023-06-28 12:15:14 +0200

    Fix - Added check for agcom attachments on page

--- src/download_agcom.py
+++ src/download_agcom.py
@@ -30,36 +30,39 @@ def main():
     if (options.out_file is None):
         parser.error("Numero di argomenti non corretto")
 
-    url = "https://www.agcom.it/provvedimenti-a-tutela-del-diritto-d-autore"
-    page = requests.get(url)
-    soup = BeautifulSoup(page.content, "html.parser")
+    curpage=1
     lastDetermina = None
-    for div in soup.findAll('div', attrs={'class':'risultato'}):
-        if lastDetermina: break
-        for p in div.findAll('p'):
-            if ((p.text.lower().find("provvedimento")==-1) and (p.text.lower().find("ordine")==-1)):continue
-            #determina = div.find(lambda tag:(tag.name=="a" and (tag.text.lower().find("determina")!=-1) or (tag.text.lower().find("delibera")!=-1)))
-            determina = div.find(lambda tag:(tag.name=="a" and tag.text.lower().find("determina")!=-1))
-            if determina:
-                lastDetermina = "https://www.agcom.it"+determina["href"]
-                break
+    while((curpage<10) and (lastDetermina is None)):        
+        url = "https://www.agcom.it/provvedimenti-a-tutela-del-diritto-d-autore?p_p_id=listapersconform_WAR_agcomlistsportlet&p_p_lifecycle=0&p_p_state=normal&p_p_mode=view&p_p_col_id=column-1&p_p_col_count=1&_listapersconform_WAR_agcomlistsportlet_numpagris=50&_listapersconform_WAR_agcomlistsportlet_curpagris={}".format(curpage)
+        page = requests.get(url)
+        soup = BeautifulSoup(page.content, "html.parser")
+        for div in soup.findAll('div', attrs={'class':'risultato'}):
+            if lastDetermina: break
+            for p in div.findAll('p'):
+                if ((p.text.lower().find("provvedimento")==-1) and (p.text.lower().find("ordine")==-1)):continue
+                    #determina = div.find(lambda tag:(tag.name=="a" and (tag.text.lower().find("determina")!=-1) or (tag.text.lower().find("delibera")!=-1)))
+                determina = div.find(lambda tag:(tag.name=="a" and tag.text.lower().find("determina")!=-1))
+                if determina:
+                    lastDetermina = "https://www.agcom.it"+determina["href"]
+                    #### Check Allegato ######
+                    page = requests.get(lastDetermina)
+                    soup = BeautifulSoup(page.content, "html.parser")
+                    # Controllo se ho trovato un Allegato B vedendo se la variabile inizializzata è stata modificata
+                    # Solo se allegatoB è stato trovato, allora lo elaboro
+                    for allegato in soup.find_all("a"):
+                        if not "Allegato B" in allegato.text:continue
+                        allegatoB = allegato["href"]
+                        break
+                    if not "www.example.com" in allegatoB:
+                        response = requests.get(allegatoB)
+                        with open(options.out_file,'wb') as f:
+                            f.write(response.content)
+                    else:
+                        lastDetermina = None
+                    break
+        curpage = curpage + 1
     if lastDetermina is None:
         return(False)
-    
-    page = requests.get(lastDetermina)
-    soup = BeautifulSoup(page.content, "html.parser")
-    for allegato in soup.find_all("a"):
-        if not "Allegato B" in allegato.text:continue
-        allegatoB = allegato["href"]
-        break
-
-# Controllo se ho trovato un Allegato B vedendo se la variabile inizializzata è stata modificata
-# Solo se allegatoB è stato trovato, allora lo elaboro
-
-    if not "www.example.com" in allegatoB:
-        response = requests.get(allegatoB)
-        with open(options.out_file,'wb') as f:
-            f.write(response.content)
 
 if __name__ == '__main__':
     main()
