4981aa147f2ae1968ae45c5fd69fa10732f6d433
Antonio Prado <antonio@prado.it>
2023-07-04 16:39:01 +0200

    Update censorship_params.sh

--- src/censorship_params.sh
+++ src/censorship_params.sh
@@ -7,7 +7,7 @@ PARSER_BIN="${ROOT_DIR}/censor_parser.py"
 WGET_BIN=$(which wget)
 
 BLACKHOLE="127.0.0.1" #Replace with the chosen IP address
-OUTPUT_FORMAT="unbound"  # Replace to "bind" or to "unbound"
+OUTPUT_FORMAT="unbound"  # Replace to "bind" or to "unbound" or to "powerdns"
 
 # Unbound params
 UNBOUND_CONF_DIR="/usr/local/etc/unbound/blacklists.d"
