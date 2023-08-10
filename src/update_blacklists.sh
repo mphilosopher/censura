3dc3abd381f9187eeec86b86e24ad446b78bb4f5
Enrico Caruso <enrico.caruso@timenet.it>
2023-08-10 14:24:49 +0200

    Fix missing automatic agcom update

--- src/update_blacklists.sh
+++ src/update_blacklists.sh
@@ -3,6 +3,7 @@
 . $(dirname "${0}")/censorship_params.sh
 
 ${ROOT_DIR}/update_cncpo.sh || true
+${ROOT_DIR}/update_agcom.sh || true
 ${ROOT_DIR}/update_consob.sh || true
 ${ROOT_DIR}/update_aams.sh  || true
 ${ROOT_DIR}/update_admt.sh  || true
