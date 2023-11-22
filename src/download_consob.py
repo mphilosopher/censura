e1f55b11a100be20159faedf796674df87987cd3
Antonio Prado <antonio@prado.it>
2023-11-22 17:10:06 +0100

    Update download_consob.py
    added verify=False on requests to deal with poor TLS certificate on consob.it

--- src/download_consob.py
+++ src/download_consob.py
@@ -24,14 +24,14 @@ def main():
     curpage=1
     urls = []
     url = "https://www.consob.it/web/area-pubblica/oscuramenti?p_p_id=com_liferay_asset_publisher_web_portlet_AssetPublisherPortlet_INSTANCE_m9PTOY4SM1GU&_com_liferay_asset_publisher_web_portlet_AssetPublisherPortlet_INSTANCE_m9PTOY4SM1GU_cur={}".format(curpage)
-    page = requests.get(url)
+    page = requests.get(url,verify=False)
     soup = BeautifulSoup(page.content, "html.parser")
     for span in soup.findAll('span', attrs={'class':'lfr-icon-menu-text'}):
         pag=span.getText()
         totpages=int(re.search(r"[0-9]{1,2}$",pag).group(0))
     while(curpage<=totpages):
         url = "https://www.consob.it/web/area-pubblica/oscuramenti?p_p_id=com_liferay_asset_publisher_web_portlet_AssetPublisherPortlet_INSTANCE_m9PTOY4SM1GU&_com_liferay_asset_publisher_web_portlet_AssetPublisherPortlet_INSTANCE_m9PTOY4SM1GU_cur={}".format(curpage)
-        page = requests.get(url)
+        page = requests.get(url,verify=False)
         soup = BeautifulSoup(page.content, "html.parser")
         for div in soup.findAll('div', attrs={'class':'divContent'}):
             for p in div.findAll(string=re.compile("Di seguito.*siti|riguardano i siti")):
